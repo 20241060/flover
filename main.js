9c74d38317e3d817af0e5860c45248437b4b7b0e
20241060 <20241060@vision.hoseo.edu>
2025-01-29 18:37:39 +0900

    다운로드 시 이미지 이름 수정

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const link = document.createElement("a");
 link.href = "https://example.com/image.jpg";
-link.download = "my-image.jpg";
+link.download = "송하영 인생네컷.jpg";
 document.body.appendChild(link);
 link.click();
 document.body.removeChild(link);

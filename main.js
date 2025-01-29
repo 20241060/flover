9a1a45d573ecbce6834e362b961d1b33a6b943b3
20241060 <20241060@vision.hoseo.edu>
2025-01-29 18:39:36 +0900

    이미지 href 수정

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const link = document.createElement("a");
-link.href = "https://example.com/image.jpg";
+link.href = "https://raw.githubusercontent.com/20241060/flover/refs/heads/main/%EC%86%A1%ED%95%98%EC%98%81%20%EC%9D%B8%EC%83%9D%EB%84%A4%EC%BB%B7.png";
 link.download = "송하영 인생네컷.jpg";
 document.body.appendChild(link);
 link.click();
